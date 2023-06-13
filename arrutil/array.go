777047faa65131f1545aefa0c5e819fb7f7a63dc
yearnfar <yearnfar@gmail.com>
2023-06-14 00:20:23 +0800

    feat: 新增NumbersToStrs、Merge

--- arrutil/array.go
+++ arrutil/array.go
@@ -1,5 +1,9 @@
 package arrutil
 
+import (
+	"fmt"
+)
+
 // InArray 判断数据是否在数组中
 func InArray[T comparable](v T, arr []T) bool {
 	for _, a := range arr {
@@ -8,4 +12,21 @@ func InArray[T comparable](v T, arr []T) bool {
 		}
 	}
 	return false
+}
+
+// NumbersToStrs 任意类型slice转为字符串数组
+func NumbersToStrs[T ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~float32 | ~float64](arr []T) []string {
+	ret := make([]string, len(arr))
+	for i := range arr {
+		ret[i] = fmt.Sprint(arr[i])
+	}
+	return ret
+}
+
+// Merge 合并两个数组
+func Merge[T any](arr1 []T, arr2 []T) []T {
+	newArr := make([]T, len(arr1)+len(arr2))
+	copy(newArr, arr1)
+	copy(newArr[len(arr1):], arr2)
+	return newArr
 }
--- arrutil/array_test.go
+++ arrutil/array_test.go
@@ -1,6 +1,9 @@
 package arrutil
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestInArray(t *testing.T) {
 	type testData[T comparable] struct {
@@ -33,4 +36,48 @@ func TestInArray(t *testing.T) {
 		}
 	}
 	t.Log("done")
+}
+
+func TestNumbersToString(t *testing.T) {
+	testData1 := []struct {
+		arr    []int
+		expect []string
+	}{
+		{[]int{1, 2, 3, 4, 5}, []string{"1", "2", "3", "4", "5"}},
+		{[]int{11, 22, 33, 44, 55}, []string{"11", "22", "33", "44", "55"}},
+	}
+	for _, item := range testData1 {
+		out := NumbersToStrs(item.arr)
+		if strings.Join(out, ",") != strings.Join(item.expect, ",") {
+			t.Fatalf("number : %v , export: %v", item.arr, item.expect)
+		}
+	}
+}
+
+func TestMerge(t *testing.T) {
+	testData1 := [][3][]string{
+		{{"你"}, {"好"}, {"你", "好"}},
+		{{"再"}, {"见"}, {"再", "见"}},
+		{{"安和"}, {"桥"}, {"安和", "桥"}},
+	}
+
+	for _, item := range testData1 {
+		out := Merge(item[0], item[1])
+		if strings.Join(out, ",") != strings.Join(item[2], ",") {
+			t.Fatalf("merge : %v and %v, export: %v", item[0], item[1], item[2])
+		}
+	}
+
+	testData2 := [][3][]int{
+		{{1}, {3, 4}, {1, 3, 4}},
+		{{2, 3}, {5}, {2, 3, 5}},
+		{{1, 2}, {3, 4}, {1, 2, 3, 4}},
+	}
+
+	for _, item := range testData2 {
+		out := Merge(item[0], item[1])
+		if strings.Join(NumbersToStrs(out), ",") != strings.Join(NumbersToStrs(item[2]), ",") {
+			t.Fatalf("merge : %v and %v, export: %v", item[0], item[1], item[2])
+		}
+	}
 }
